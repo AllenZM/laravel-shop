f2d16b3cc451bb96defc5e3cf30a03dfdf4dba06
董坤鸿 <249529979@qq.com>
2018-06-18 15:58:01 +0800

    辅助函数

---
+++ bootstrap/helpers.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Name: 自定义辅助函数.
+ * User: 董坤鸿
+ * Date: 2018/6/18
+ * Time: 下午3:50
+ */
+
+if (! function_exists('test_helper')) {
+
+    function test_helper()
+    {
+        return 'Ok';
+    }
+}
