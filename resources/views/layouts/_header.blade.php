a317beba6dd0426cf16191b74db18f4e974fc2a5
董坤鸿 <249529979@qq.com>
2018-06-18 17:43:14 +0800

    登录与注册

--- resources/views/layouts/_header.blade.php
+++ resources/views/layouts/_header.blade.php
@@ -8,16 +8,35 @@
         </button>
 
         <div class="collapse navbar-collapse" id="navbarSupportedContent">
-            <!-- Left Side Of Navbar -->
+            <!-- 导航左侧 -->
             <ul class="navbar-nav mr-auto">
 
             </ul>
 
-            <!-- Right Side Of Navbar -->
+            <!-- 导航右侧 -->
             <ul class="navbar-nav ml-auto">
-                <!-- Authentication Links -->
-                <li><a class="nav-link" href="#">登录</a></li>
-                <li><a class="nav-link" href="#">注册</a></li>
+                <!-- 认证链接开始 -->
+                @guest
+                    <li><a class="nav-link" href="{{ route('login') }}">登录</a></li>
+                    <li><a class="nav-link" href="{{ route('register') }}">注册</a></li>
+                @else
+                    <li class="nav-item dropdown">
+                        <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+                            <span class="user-avatar pull-left" style="margin-right:8px; margin-top:-5px;">
+                                <img src="https://fsdhubcdn.phphub.org/uploads/images/201709/20/1/PtDKbASVcz.png?imageView2/1/w/60/h/60" class="img-responsive img-circle" style="border-radius:50%; width: 30px; height: 30px">
+                            </span>
+                            {{ Auth::user()->name }} <span class="caret"></span>
+                        </a>
+
+                        <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                            <a class="dropdown-item" href="{{ route('logout') }}" onclick="event.preventDefault(); document.getElementById('logout-form').submit();">退出登录</a>
+                            <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                                @csrf
+                            </form>
+                        </div>
+                    </li>
+                @endguest
+                <!-- 认证链接结束 -->
             </ul>
         </div>
     </div>
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -6,7 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
-    <title>@yield('title', 'Laravel Shop') - 涂呀网商城</title>
+    <title>@yield('title', '涂呀网') - 涂呀网商城</title>
     <!-- 样式 -->
     <link href="{{ mix('css/app.css') }}" rel="stylesheet">
 </head>
