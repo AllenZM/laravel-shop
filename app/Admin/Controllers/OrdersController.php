0ebc0681657dc5a750aae227407b45ff2c8b0408
董坤鸿 <249529979@qq.com>
2018-07-02 18:06:05 +0800

    管理后台 - 订单发货

--- app/Admin/Controllers/OrdersController.php
+++ app/Admin/Controllers/OrdersController.php
@@ -8,14 +8,16 @@
 
 namespace App\Admin\Controllers;
 
+use App\Exceptions\InvalidRequestException;
+use Illuminate\Http\Request;
 use App\Models\Order;
-
 use Encore\Admin\Form;
 use Encore\Admin\Grid;
 use Encore\Admin\Facades\Admin;
 use Encore\Admin\Layout\Content;
 use App\Http\Controllers\Controller;
 use Encore\Admin\Controllers\ModelForm;
+use Illuminate\Support\Facades\Auth;
 
 class OrdersController extends Controller
 {
@@ -85,6 +87,43 @@ class OrdersController extends Controller
             // body 方法可以接受 Laravel 的试图为参数
             $content->body(view('admin.orders.show', ['order' => $order]));
         });
+    }
+
+    /**
+     * 订单发货
+     *
+     * @param Order $order
+     * @param Request $request
+     * @return \Illuminate\Http\RedirectResponse
+     * @throws InvalidRequestException
+     */
+    public function ship(Order $order, Request $request)
+    {
+        // 判断当前订单是否已经支付
+        if (!$order->paid_at){
+            throw new InvalidRequestException('该订单未支付');
+        }
+        // 判断当前订单发货状态是否为未发货
+        if ($order->ship_status !== Order::SHIP_STATUS_PENDING){
+            throw new InvalidRequestException('该订单已发货');
+        }
+        // Laravel 5.5 之后 validate 方法可以返回校验过的值
+        $data = $this->validate($request, [
+            'express_company' => ['required'],
+            'express_no'    => ['required'],
+        ],[],[
+            'express_company' => '物流公司',
+            'express_no' => '物流单号',
+        ]);
+        // 将订单发货状态改为已发货，并存入物流信息
+        $order->update([
+            'ship_status' => Order::SHIP_STATUS_DELIVERED,
+            // 我们在 Order 模型的 $casts 属性里指明了 ship_data 是一个数组
+            // 因此这里可以直接吧数组传递过去
+            'ship_data' => $data,
+        ]);
+        // 妇女会上一页
+        return redirect()->back();
     }
 
     /**
--- app/Admin/routes.php
+++ app/Admin/routes.php
@@ -17,15 +17,25 @@ Route::group([
     $router->get('users', 'UsersController@index');
 
     // 产品管理/商品管理
+    // 商品列表
     $router->get('products', 'ProductsController@index');
+    // 创建商品
     $router->get('products/create', 'ProductsController@create');
+    // 插入商品
     $router->post('products', 'ProductsController@store');
+    // 编辑商品
     $router->get('products/{id}/edit', 'ProductsController@edit');
+    // 修改商品
     $router->put('products/{id}', 'ProductsController@update');
+    // 删除商品
     $router->delete('products/{id}', 'ProductsController@destroy');
 
     // 产品管理/订单管理
+    // 订单列表
     $router->get('orders', 'OrdersController@index')->name('admin.orders.index');
+    // 订单详情
     $router->get('orders/{order}', 'OrdersController@show')->name('admin.orders.show');
+    // 订单发货
+    $router->post('orders/{order}/ship', 'OrdersController@ship')->name('admin.orders.ship');
 
 });
